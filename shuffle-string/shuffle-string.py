1b946358cd45e6a939de463eabb6e64161fc2b25
Thomas H <t.hanna3332@gmail.com>
2021-09-05 13:23:10 -0400

    Time: 60 ms (34.57%), Space: 14.2 MB (54.03%) - LeetHub

--- shuffle-string/shuffle-string.py
+++ shuffle-string/shuffle-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        for index, value in enumerate(indices):
+        
             ans = ""
             for i in range(len(indices)):
                 ans += s[indices.index(i)]
