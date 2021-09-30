51a048345935cd202e9099939564af5be2f6c25e
Thomas H <t.hanna3332@gmail.com>
2021-09-30 18:27:49 -0400

    Time: 32 ms (69.48%), Space: 14.2 MB (69.87%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,19 +1,16 @@
 
 
 class Solution:
-   
-    def fib(self, n: int, **dict ) -> int:
-        d = {}
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        if n in d:
-            return d[n]
-        else: 
-            d[n] = self.fib(n-1) + self.fib(n-2)
-        return d[n]
-        
-        
     
+    def fib(self, n: int) -> int:
+        
+        
+        f = [0, 1]
+     
+     
+        for i in range(2, n+1):
+            f.append(f[i-1] + f[i-2])
+        return f[n]
+     
+      
         
