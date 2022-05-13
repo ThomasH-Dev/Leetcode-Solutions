3e38e032bca36e6bc88eba5cf76b718635839260
Thomas H <t.hanna3332@gmail.com>
2022-05-13 16:40:35 -0400

    Time: 57 ms (70.19%), Space: 14.8 MB (25.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,20 +3,9 @@ class Solution:
         d = {}
         if len(s) != len(t):
             return False
-        for i in range(len(s)):
-            if s[i] not in d:
-                d[s[i]] = 1
-            else:
-                d[s[i]] +=1
-        print(d)
-        for i in range(len(t)):
-            if t[i] not in d:
-                return False
-            else:
-                d[t[i]] -= 1
-                if d[t[i]] < 0:
-                    return False
-        return True
-        
-        
-                
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        if s == t:
+            return True
+        else:
+            return False
